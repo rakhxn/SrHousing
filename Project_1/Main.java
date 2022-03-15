117e78eb663cb973d70346d8d2cd0df2e52b3407
Rajiv Bharrat <rajivkhan6@gmail.com>
2022-03-15 18:18:35 -0400

    windows changes

--- Project_1/Main.java
+++ Project_1/Main.java
@@ -1,11 +1,13 @@
 package Project_1;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -24,8 +26,15 @@ public class Main extends Application{
 
         button = new Button("Click me");
         button.setOnAction(e -> {
-            boolean result = ConfirmBox.display("Sr. Housing", "Are you sure you wanna suck me off");
-            System.out.println(result);
+            boolean result = ConfirmBox.display("Sr. Housing", "eirutghiuaeriuh");
+            if (result == true){
+                Confirmed.display("Confirmed!", "Updated!");
+                System.out.println("True!");
+
+            }
+            if (result == false){
+                System.out.println("Fuck no, gtfo!");
+            }
         });
 
         StackPane layout = new StackPane();
